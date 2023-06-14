337fbef0b25786b6097d15993a053dc7afe5bea6
Joel <joel.r.maxwell@gmail.com>
2023-06-14 16:20:43 +1000

    example file

---
+++ src/example.m
@@ -0,0 +1,14 @@
+% File : example
+% Description : A simple example file
+
+% --- Dummy Code ---
+
+printHello()
+
+
+% --- Dummy Functions ---
+
+% printHello : Prints hello 
+function printHello()
+    fprintf("\nHello, World!\n");
+end
