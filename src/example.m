3ce6fdcc64209c535b67bd46456e414238f19e3e
ericp2500 <65203141+ericp2500@users.noreply.github.com>
2023-06-23 19:57:24 +1000

    Update example.m

--- src/example.m
+++ src/example.m
@@ -1,9 +1,20 @@
-% File : example
-% Description : A simple example file
+% File : Example Model
+% Description : Example of Model with Dummy Variables/Values Used
+
+
+% --- Dummy Variable ---
+Laser_Energy_Input = 0
+Laser_Energy_Output = 0
+
+
+Generator_Efficiency = 0.5
+
+
 
 % --- Dummy Code ---
+Reaction_Energy_Output = Reaction_Energy*Number_of_Reactions_per_Second
+Generator_Energy_Output = Generator_Efficiency*Reaction_Energy_Output
 
-printHello()
 
 
 % --- Dummy Functions ---
